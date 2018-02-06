9673f615049fdcd245cac6a4b9d90932c8d55924
russellf9 <russell@factornine.co.uk>
2018-02-06 21:59:02 +0000

    Update app

--- app.js
+++ app.js
@@ -3,24 +3,28 @@ const fetch = require('node-fetch');
 
 const stringify = x => { console.log(JSON.stringify(x, null, 2))}
 
+// const num = 100
 
+const getURL = (num) => { return  `https://randomuser.me/api/?results=${num}&nat=gb&seed=foobar-99`}
 
-const URL = 'https://randomuser.me/api/?results=10&nat=gb&seed=foobar-99'
-
-const fetchPeopleData = () => {
-  return fetch(URL)
+const fetchPeopleData = (num = 10) => {
+  return fetch(getURL(num))
     .then(response => response.json())
     .then(json => { return json.results } )
 }
 
-
 fetchPeopleData()
 
-
-
+const singlePersonUrl = 'https://randomuser.me/api/?results=1&nat=gb&seed=foobar-98';
+const getSinglePerson = () => {
+  return fetch(singlePersonUrl)
+  .then(response => response.json())
+  .then(json => { return json.results } )
+}
 
 
 module.exports =  {
   fetchPeopleData: fetchPeopleData,
-  stringify: stringify
+  stringify: stringify,
+  getSinglePerson: getSinglePerson
 }
